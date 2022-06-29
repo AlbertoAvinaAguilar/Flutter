d4cfe7f9afd376c37538e947822a11961d015190
Alberto Aviña <iscalrj@hotmail.com>
2022-06-29 13:39:53 -0500

    Se separo la logica del widget MaterialApp y cargamos el home de otra pantalla

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //el stateleswidget es estatico no cambia
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Text('Hola Mundo'),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
