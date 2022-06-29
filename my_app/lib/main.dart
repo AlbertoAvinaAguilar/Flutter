a8d1601aaf1cebeaa08c1f15074921266980c71b
Alberto Aviña <iscalrj@hotmail.com>
2022-06-29 11:54:38 -0500

    este cambio es solamente visual

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Jala perron:',
+              'Jala perron 2:',
             ),
             Text(
               '$_counter',
