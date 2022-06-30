d37960e47fd3b36b3a26f4360a9db3529d59039e
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-06-30 08:14:58 -0500

    Se agregaron dos widgets mas y probamos la ubicacion de los mmismos mediante los mainAxis

--- my_app/lib/screens/home_screen.dart
+++ my_app/lib/screens/home_screen.dart
@@ -5,8 +5,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hola desde home screen'),
-    );
+    TextStyle fontSize30 = const TextStyle(fontSize: 30);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('HomeScreen'),
+          elevation: 3.0,
+        ),
+        body: Center(
+          //Alinea el elemento al centro clumna horizontal, row vertical
+          child: Column(
+            //En columna este se basa en orientacion horizontal
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min, //Vertical
+            mainAxisAlignment: MainAxisAlignment.center, //Coulmna en vertical
+            children: <Widget>[
+              Text('Contador', style: fontSize30),
+              Text('Otro texto', style: fontSize30),
+              const Text('0',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500)),
+            ],
+          ),
+        ));
   }
 }
