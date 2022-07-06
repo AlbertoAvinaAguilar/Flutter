b0601c2a4ac433e196bb4519fe9b236c79a2a16f
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-07-05 21:35:35 -0500

    Se realizo un widget statefulwidget y ademas se implemnto un widget externo y se realizo una funcion que manìpula la funcion de los botones

--- my_app/lib/screens/home_screen.dart
+++ my_app/lib/screens/home_screen.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //Aqui van las propiedades que vamos a manipular con e stateful widget en el setstage
+  int counter = 0;
+
+  void operacionesButtons(int option) {
+    if (option == 1) {
+      //aumentar
+      counter++;
+    } else if (option == 2) {
+      //restar
+      counter--;
+    } else {
+      //en 0
+      counter = 0;
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    TextStyle fontSize30 = const TextStyle(fontSize: 30);
+    const fontSize = TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
 
     return Scaffold(
         appBar: AppBar(
@@ -20,12 +42,44 @@ class HomeScreen extends StatelessWidget {
             //mainAxisSize: MainAxisSize.min, //Vertical
             mainAxisAlignment: MainAxisAlignment.center, //Coulmna en vertical
             children: <Widget>[
-              Text('Contador', style: fontSize30),
-              Text('Otro texto', style: fontSize30),
-              const Text('0',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500)),
+              const Text('Contador', style: fontSize),
+              const Text('Otro texto', style: fontSize),
+              Text('$counter', style: fontSize),
             ],
           ),
-        ));
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton:
+            CustomFloatingActions(increaseFn: operacionesButtons));
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function increaseFn;
+
+  const CustomFloatingActions({
+    Key? key,
+    required this.increaseFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_plus_1_outlined),
+          onPressed: () => increaseFn(1),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_outlined),
+          onPressed: () => increaseFn(3),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => increaseFn(2),
+        ),
+      ],
+    );
   }
 }
