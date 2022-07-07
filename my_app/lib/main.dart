707b508da5b52d21a750b39079a8920e6985f905
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-07-06 20:39:56 -0500

    trabajando con listview en flutter

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/home_screen.dart';
+import 'package:my_app/screens/listview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      //home: HomeScreen(),
+      home: Listview1Screen(),
       debugShowCheckedModeBanner: false,
     );
   }
