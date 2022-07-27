609dd38e620d475535736f175a26e51c5a1e2025
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-07-26 21:29:00 -0500

    Agregamos una dependencia para random palabras, creamos un statefulwidget y un listview con funcionalidad

--- myapp3/flutter_practice/lib/main.dart
+++ myapp3/flutter_practice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,16 +10,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //const mensaje = 'Hola Mundo'; //Creacion de una variable
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          //child: Text(mensaje), //pasamos la variable
+          child:
+              RandomWords(), //Utilizamos un statefulwidget que obtiene una palabra en ingles aleatoria
         ),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  //_ indica privacidad para el solo uso en la clase
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random(); // NEW
+    //return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        //isOdd retorna una división cuando i sea impar 1,3,5,7,9,11 NOTA inicia en 0
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2; // divide el i / 2 y lo pone en numero entero
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
     );
   }
 }
