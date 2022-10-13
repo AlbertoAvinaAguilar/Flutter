62ac724b98523d64a3874f7aab7659277cf96d1f
Alberto Aviña <iscalrj@hotmail.com>
2022-10-13 15:10:56 -0500

    widget text custom

--- my_app/lib/pages/home_page.dart
+++ my_app/lib/pages/home_page.dart
@@ -9,25 +9,36 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Lista de usuarios")),
       body: const Center(
-        child: Text("Bienvenidos aQUI",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.amberAccent,
-                backgroundColor: Colors.black,
-                fontSize: 25.0,
-                fontWeight: FontWeight.w800,
-                fontStyle: FontStyle.italic,
-                letterSpacing: 5.0,
-                wordSpacing: 5.0,
-                textBaseline: TextBaseline.alphabetic,
-                height: 10.0,
-                shadows: [
-                  Shadow(
-                      color: Color.fromARGB(255, 223, 223, 223),
-                      blurRadius: 16,
-                      offset: Offset(1, 1))
-                ])),
+        child: CustomText(),
       ),
     );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  const CustomText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Bienvenidos aQUI",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors.amberAccent,
+            backgroundColor: Colors.black,
+            fontSize: 25.0,
+            fontWeight: FontWeight.w800,
+            fontStyle: FontStyle.italic,
+            letterSpacing: 5.0,
+            wordSpacing: 5.0,
+            textBaseline: TextBaseline.alphabetic,
+            height: 10.0,
+            shadows: [
+              Shadow(
+                  color: Color.fromARGB(255, 223, 223, 223),
+                  blurRadius: 16,
+                  offset: Offset(1, 1))
+            ]));
   }
 }
