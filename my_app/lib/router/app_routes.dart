f780a98185f2ce647a0798b51c5d20d512f6a3d8
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-10-13 10:44:07 -0500

    creacion rama dev y homepage

--- my_app/lib/router/app_routes.dart
+++ my_app/lib/router/app_routes.dart
@@ -5,7 +5,8 @@ class AppRoutes {
   static const initialRoute = 'inicioSplash';
 
   static Map<String, Widget Function(BuildContext)> routes = {
-    'inicioSplash': (BuildContext context) => const SplashScreen(),
+    'inicioSplash': (BuildContext context) => const HomePage(),
+    // 'inicioSplash': (BuildContext context) => const SplashScreen(),
     'homeLogin': (BuildContext context) => const HomeScreen(),
     'registroCuenta': (BuildContext context) => const RegistroCuenta(),
     'recuperarCuenta': (BuildContext context) => const RecuperarCuenta(),
--- my_app/lib/screens/screens.dart
+++ my_app/lib/screens/screens.dart
@@ -4,3 +4,4 @@ export 'package:my_app/screens/registro_cuenta_screen.dart';
 export 'package:my_app/screens/recuperar_cuenta_screen.dart';
 export 'package:my_app/screens/registro_cuenta_screen.dart';
 export 'package:my_app/router/app_routes.dart';
+export 'package:my_app/pages/home_page.dart';
