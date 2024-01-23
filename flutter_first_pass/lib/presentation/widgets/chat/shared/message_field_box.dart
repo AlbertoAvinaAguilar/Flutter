05313e732c124452c9abb5c714d0db49b609dab2
AlbertoAvinaAguilar <99748800+AlbertoAvinaAguilar@users.noreply.github.com>
2024-01-23 14:25:56 -0600

    diseño terminado, textformfield
    focus, keyboard, texformfiel controller,

--- flutter_first_pass/lib/presentation/widgets/chat/shared/message_field_box.dart
+++ flutter_first_pass/lib/presentation/widgets/chat/shared/message_field_box.dart
@@ -5,28 +5,45 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController =
+        TextEditingController(); //Nos permite hacer una referencia o un binding a nuestro elemento textformfield
+    final focusNode = FocusNode(); //Referencia al edittext
+
     final outlineInputBorder = UnderlineInputBorder(
-      borderSide: const BorderSide(color: Colors.transparent),
-      borderRadius: BorderRadius.circular(40));
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
-      enabledBorder: outlineInputBorder,
-      focusedBorder: outlineInputBorder,
-      filled: true,
-      suffixIcon: IconButton(icon: const Icon(Icons.send),
-      onPressed: () {
-        print('Presed');
-      },
-      )
-    );  
+        hintText: 'Captura tu mensaje',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: () {
+            final textValue = textController.value.text;
+            textController.clear();
+            print('Presed');
+          },
+        ));
 
-    return TextFormField(decoration: inputDecoration,
-    onFieldSubmitted: (value) {
-      print('Submit $value');
-    },
-    onChanged: (value) {
-      print('Changed $value');
-    },
+    return TextFormField(
+      keyboardAppearance: Brightness.dark,
+      keyboardType: TextInputType.number,
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+      onChanged: (value) {
+        print('Changed $value');
+      },
     );
   }
 }
